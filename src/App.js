6013bd6a401176dabb8fd05d8db9617ce0bd47b8
grant-create <grant.s.wilkie@gmail.com>
2021-06-22 15:04:50 -0700

    starting gif

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+import Home from './Home'
+
+
+export default class App extends Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+
+}
